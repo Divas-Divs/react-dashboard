a0e5d1a9c861eb7239610b78cb7872d7d487378d
schererm <118745887+schererm@users.noreply.github.com>
2026-03-15 02:43:07 +0000

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import TriviaGame from "./components/TriviaGame";
 import DateInfo from "./components/DateInfo";
 import Facts from "./components/Facts";
 import './App.css'
+import logo from './assets/trivia-diva-logo.png'
 
 function App() {
   return (
     <>
-      <h1>Trivia Diva</h1>
+      <h1 style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
+  <img src={logo} alt="" style={{ height: '1em', width: 'auto' }} />
+  Trivia Diva
+</h1>
       <Facts />
       <div className="fact-container">
         <DateInfo />
